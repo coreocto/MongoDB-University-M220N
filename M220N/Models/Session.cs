e67721d1993682147d8591a2d4987c41c08bee0e
Core Octo <coreocto@hotmail.com>
2021-03-03 23:27:00 +0000

    Add project files.

---
+++ M220N/Models/Session.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace M220N.Models
+{
+    [BsonIgnoreExtraElements]
+    public class Session
+    {
+        
+        [BsonElement("user_id")]
+        public string UserId { get; set; }
+
+        public string Jwt { get; set; }
+    }
+}
